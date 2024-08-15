58bd716be4f51cb37783f28a1a3a6cf5c660ce72
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-15 22:44:53 +0200

    h

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".container img");
-    const totalDuration = 1200000;
-    const durationPerElement = totalDuration/images.length;
+    const images = document.querySelectorAll("img");
+    const durationPerElement = 8; // Duration for each image in seconds
 
-    images.forEach((image, index)=>{
-        image.classList.add("anim");
-        setTimeout(()=>{
-            // image.classList.remove("anim");
-            
-        }, index*durationPerElement)
-    })
-
-    // for(let i= 0; i<images.length; i++){
-    //     images[i].classList.remove("hide");
-        
-    //      setTimeout(()=>{
-    //         images[i].classList.add("hide");
-    //      }, i * durationPerElement)}
-  });
-
-  //animation duration has to be set to a value and that value has to be devided by total length of images
-  //set duration variable to total duration divided by length of images
-//    then set that duration value  to style property of each image
-// but need to find out how to start each element's starting point and ending point
+    images.forEach((image, index) => {
+        image.classList.add("anim"); // Add the animation class to each image
+        image.style.animationDelay = `${index * durationPerElement}s`; // Set the delay for each image
+    });
+});
