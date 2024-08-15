23e478778d1e515084e28cd6f05313cfbe272d82
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-15 22:15:51 +0200

    animation in progress

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll(".container img");
-    const count = 0;
-    // for(let i=0; i<images.length; i++){
-    //     images[i].classList.add("anim")
-    // }
-    // images.forEach(image=>{
-    //     image.classList.add("anim");
-    // })
+    const totalDuration = 1200000;
+    const durationPerElement = totalDuration/images.length;
+
+    images.forEach((image, index)=>{
+        image.classList.add("anim");
+        setTimeout(()=>{
+            // image.classList.remove("anim");
+            
+        }, index*durationPerElement)
+    })
+
+    // for(let i= 0; i<images.length; i++){
+    //     images[i].classList.remove("hide");
+        
+    //      setTimeout(()=>{
+    //         images[i].classList.add("hide");
+    //      }, i * durationPerElement)}
   });
+
+  //animation duration has to be set to a value and that value has to be devided by total length of images
+  //set duration variable to total duration divided by length of images
+//    then set that duration value  to style property of each image
+// but need to find out how to start each element's starting point and ending point
