9e9b40fc8320e55e97ad384980cd94367bac1e35
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-15 19:53:53 +0200

    class added that has been made for a carousal

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const container = document.querySelector(".container");
-    const images = container.querySelectorAll("img");
-    const count = 0;
-    images.forEach(image=>{
-        image.classList.add("hide");
-    })
 
-})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".container img");
+    const count = 0;
+    // for(let i=0; i<images.length; i++){
+    //     images[i].classList.add("anim")
+    // }
+    // images.forEach(image=>{
+    //     image.classList.add("anim");
+    // })
+  });
