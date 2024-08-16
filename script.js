eb71bc82f647b62171c1ab88d399b73669b29633
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-16 09:32:53 +0200

    1

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll(".container img");
-    const totalDuration = 48; 
+    const totalDuration = 20; 
     const durationPerElement = totalDuration / images.length; 
+    // function animationloop(){
 
-    // images.forEach((image, index) => {
-    //     // image.classList.add("anim"); 
-        
-    //     image.style.animationDuration = `${durationPerElement}s`; // Stagger the start time for each image
-    //     image.style.animationDelay = `${index * durationPerElement}s`; // Stagger the start time for each image
-        
-    // });
-    for(let i=0; i<images.length; i++){
-        images[i].style.animationDuration = `${durationPerElement}s`; // Stagger the start time for each image
-        images[i].style.animationDelay = `${i * durationPerElement}s`; // Stagger the start time for each image
+        images.forEach((image, index) => {
+                image.classList.add("anim"); 
+                image.style.animationDuration = `${durationPerElement}s`; 
+                image.style.animationDelay = `${index * durationPerElement}s`; 
+                console.log(index)
+                setTimeout(()=>{
+                    image.classList.remove("anim")
+                }, durationPerElement * 1000);
+      
+        });
+    // }
+    // animationloop();
 
-    }
-});
+    });
