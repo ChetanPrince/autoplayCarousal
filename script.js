1a440d204af580721f893b97c974f3a9fa5aaf5b
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-16 14:20:42 +0200

    4

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
 // document.addEventListener("DOMContentLoaded", function() {
     const slides = document.querySelectorAll(".slide");
-    // const totalDuration = 20; 
-    // const durationPerElement = totalDuration / slides.length; 
+    const totalDuration = 48; 
+    const durationPerElement = totalDuration / slides.length; 
    
     let counter = 0;
 
 slides.forEach((slide, index) => {
+    
+    // slide.style.display = "block";
+    slide.style.left = `${index * 100}%`;
 
-    slide.style.left = `${index * 100}%`
+    slide.classList.add("anim")
+    slide.style.animationDuration = `${durationPerElement}s`;
+    // slide.style.animationDelay = `${index * durationPerElement}s`;
+
 
 })
-const slideImage = () =>{
-    slides.forEach(
-        (slide)=>{
-            slide.style.transform =`translateX(-${counter * 100}%)`
-        }
-    )
-}
-   const goPrev = ()=>{
-    counter--;
-    slideImage();
-   }
-
-   const goNext = ()=>{
-    counter++;
-    slideImage();
-   }
-
-
-   slideImage();
-
-    // });
