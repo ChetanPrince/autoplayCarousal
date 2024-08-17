187d8aff02eeb5595e6b74a63505c662d845ada8
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-17 14:35:50 +0200

    basic prev next buttons with circles representing current page updated

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-// document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll(".slide");
-    const totalDuration = 48; 
-    const durationPerElement = totalDuration / slides.length; 
-   
-    let counter = 0;
-
-slides.forEach((slide, index) => {
-
-    slide.style.left = `${index * 100}%`;
-
-})
-
-for(let i = 0; i<slides.length; i++){
-    slides[i].classList.add("anim")
-    
-    setTimeout(()=>{
-        slides[i].classList.remove("anim");
-        slides[i].style.display = "none";
-    
-    }, durationPerElement*1000)
-}
-
-
-    // slide.style.animationDuration = `${durationPerElement}s`;
-    // slide.style.animationDelay = `${index * durationPerElement}s`;
-
-
-
+const prev = documment.querySelector(".nav .prev");
+const next = document.querySelector(".nav .next");
+const container = document.querySelector(".container");
+const images = document.querySelectorAll(".container-image img");
+const buttons = document.querySelectorAll(".buttons-div .button") 
